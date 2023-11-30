a0b4ad773f7773bc22fe4b0d8668ca1f5a41a0b3
Kiara <frida.shivaa@gmail.com>
2023-11-30 16:56:19 +0100

    add basic table

--- src/main/java/com/mkyong/controller/WelcomeController.java
+++ src/main/java/com/mkyong/controller/WelcomeController.java
@@ -1,6 +1,9 @@
 package com.mkyong.controller;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,11 +14,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WelcomeController {
     private Date dat = new Date();
+    private int openingHour = 10;
+    private int closingHour = 20;
 
     @GetMapping("/welcome")
     public String mainWithParam(
             @RequestParam(name = "name", required = false, defaultValue = "") String name, Model model) {
+        List<Date> colums = initTimeslotsFromSelectedTime(dat.getTime());
         model.addAttribute("date", dat);
+        model.addAttribute("dates", colums);
+        model.addAttribute("alleys", alleys(colums.size()));
         return "welcome";
     }
 
@@ -34,5 +42,70 @@ public class WelcomeController {
         System.out.println(type);
 
         return "welcome";
+    }
+
+    @PostMapping("/welcome/d")
+    public String bokTimeslot(Model model,
+            @RequestParam String slot) {
+        System.out.println(slot);
+
+        return "welcome";
+    }
+
+    private List<Integer> alleys(int count) {
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            arr.add(i);
+        }
+        return arr;
+    }
+
+    private List<Date> initTimeslotsFromSelectedTime(long date) {
+        long selected = getCleanDate(1701435600);
+        List<Date> arr = new ArrayList<>();
+        int openingTime = 13;
+        int closingTime = 13;
+
+        int hour = 1000 * 60 * 60 * 60;
+
+        for (int i = 0; i < 7; i++) {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(new Date(date));
+            cal.add(Calendar.HOUR_OF_DAY, 1 * i);
+            arr.add(cleanDate(cal.getTime()));
+        }
+        return arr;
+    }
+
+    public Date cleanDate(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        if (calendar.get(Calendar.MINUTE) > 15 && calendar.get(Calendar.MINUTE) < 45) {
+            calendar.set(Calendar.MINUTE, 30);
+        } else {
+            calendar.set(Calendar.MINUTE, 0);
+        }
+        return calendar.getTime();
+    }
+
+    public long getCleanDate(long timestamp) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(timestamp * 1000);
+        calendar.set(Calendar.HOUR_OF_DAY, openingHour);
+        if (calendar.get(Calendar.MINUTE) > 15 && calendar.get(Calendar.MINUTE) < 45) {
+            calendar.set(Calendar.MINUTE, 30);
+        } else {
+            calendar.set(Calendar.MINUTE, 0);
+        }
+        return calendar.getTime().getTime();
+    }
+
+    private List<Integer> initArrTimesTaken() {
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1701340620); // Nov 30 2023 10:37
+        arr.add(1701344220); // Nov 30 2023 11:37
+        arr.add(1701257820); // Nov 29 2023 11:37
+        arr.add(1701435600); // Dec 01 2023 13:00
+        return arr;
     }
 }
